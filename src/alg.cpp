0172733542d8570038b2f903ea205cbebf137da3
Анастасия Васильева <99146036+vasnastia@users.noreply.github.com>
2022-02-17 20:44:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,10 +45,9 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (uint64_t i = 0; i < hbound; i++) {
+  for (int i = 0; i < hbound; i++) {
     if (checkPrime(i) == true) {
       sum += i;
-      i++;
     }
   }
   return sum;
